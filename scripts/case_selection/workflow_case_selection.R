5a497340c06c1696e18f5195c578b5ffc6932871
alexpaynter <alexcpaynter@gmail.com>
2023-03-22 10:14:34 -0700

    Revert "Cohort/site error logic pasted from phase (redundant check with perform_case_selection.R"
    This reverts commit 4525f764b84d8e73074d02541f1d1b870b4e5e98.

--- scripts/case_selection/workflow_case_selection.R
+++ scripts/case_selection/workflow_case_selection.R
@@ -39,19 +39,17 @@ save_synapse <- opt$save_synapse
 phase_str <- paste0(names(config$phase), collapse = ", ")
 waitifnot(is.element(phase, names(config$phase)),
           msg = c(glue("Error: phase {phase} is not valid.  Valid values: {phase_str}"),
-                  "Usage: Rscript perform_case_selection.R -h"))
+                  "Usage: Rscript workflow_case_selection.R -h"))
 
-cohort_in_config <- names(config$phase[[phase]]$cohort)
-cohort_str <- paste0(cohort_in_config, collapse = ", ")
-waitifnot(is.element(cohort, cohort_in_config),
+cohort_str <- paste0(names(config$phase[[phase]]$cohort), collapse = ", ")
+waitifnot(is.element(phase, names(config$phase)),
           msg = c(glue("Error: cohort {cohort} is not valid for phase {phase}.  Valid values: {cohort_str}"),
-                  "Usage: Rscript perform_case_selection.R -h"))
+                  "Usage: Rscript workflow_case_selection.R -h"))
 
-sites_in_config <- names(config$phase[[phase]]$cohort[[cohort]]$site)
-site_str <- paste0(sites_in_config, collapse = ", ")
-waitifnot(is.element(site, sites_in_config),
+site_str <- paste0(names(config$phase[[phase]]$cohort[[cohort]]$site), collapse = ", ")
+waitifnot(is.element(phase, names(config$phase)),
           msg = c(glue("Error: site {site} is not valid for phase {phase} and cohort {cohort}.  Valid values: {site_str}"),
-                  "Usage: Rscript perform_case_selection.R -h"))
+                  "Usage: Rscript workflow_case_selection.R -h"))
 
 # setup ----------------------------
 
