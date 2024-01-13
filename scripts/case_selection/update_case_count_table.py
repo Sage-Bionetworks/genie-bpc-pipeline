bde38db88dba44602ccfd544b7ee5c69ec84394e
Thomas Yu <thomas.yu@sagebase.org>
2024-01-12 21:42:11 -0800

    Lint

--- scripts/case_selection/update_case_count_table.py
+++ scripts/case_selection/update_case_count_table.py
@@ -16,17 +16,28 @@ tic = time.time()
 workdir = "."
 if not os.path.exists("config.yaml"):
     workdir = "/usr/local/src/myscripts"
-with open(f"{workdir}/config.yaml", 'r') as stream:
+with open(f"{workdir}/config.yaml", "r") as stream:
     config = yaml.safe_load(stream)
 print(config)
 # user input --------------------------
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--save_synapse", action='store_true', default=False, 
-                    help="Save updated counts on Synapse")
-parser.add_argument("-c", "--comment", type=str,
-                    help="Comment for new table snapshot version")
-parser.add_argument("-a", "--synapse_auth", type=str, default=None,
-                    help="Path to .synapseConfig file or Synapse PAT (default: normal synapse login behavior")
+parser.add_argument(
+    "-s",
+    "--save_synapse",
+    action="store_true",
+    default=False,
+    help="Save updated counts on Synapse",
+)
+parser.add_argument(
+    "-c", "--comment", type=str, help="Comment for new table snapshot version"
+)
+parser.add_argument(
+    "-a",
+    "--synapse_auth",
+    type=str,
+    default=None,
+    help="Path to .synapseConfig file or Synapse PAT (default: normal synapse login behavior",
+)
 args = parser.parse_args()
 
 save_synapse = args.save_synapse
@@ -37,25 +48,30 @@ auth = args.synapse_auth
 syn = synapseclient.Synapse()
 syn.login()
 
-def get_current_production_record_count(synid_table_patient, cohort, phase, site = None):
+
+def get_current_production_record_count(synid_table_patient, cohort, phase, site=None):
     syn = synapseclient.login()
 
-    if cohort in ['NSCLC','CRC'] and phase == "2":
+    if cohort in ["NSCLC", "CRC"] and phase == "2":
         cohort = f"{cohort}{phase}"
     if site is None:
         query = f"SELECT record_id FROM {synid_table_patient} WHERE cohort = '{cohort}'"
     else:
         query = f"SELECT record_id FROM {synid_table_patient} WHERE cohort = '{cohort}' AND redcap_data_access_group = '{site}'"
-    
+
     results = syn.tableQuery(query)
     record_ids = results.asDataFrame()
 
     return len(record_ids)
 
+
 def create_synapse_table_snapshot(table_id, comment):
-    snapshot = syn.restPOST(f"/entity/{table_id}/table/transaction/commit", 
-                            body = f"{{'changes': [], 'snapshotOptions': {{'snapshotComment': '{comment}'}}}}")
-    return snapshot['snapshotVersionNumber']
+    snapshot = syn.restPOST(
+        f"/entity/{table_id}/table/transaction/commit",
+        body=f"{{'changes': [], 'snapshotOptions': {{'snapshotComment': '{comment}'}}}}",
+    )
+    return snapshot["snapshotVersionNumber"]
+
 
 def clear_synapse_table(table_id):
     syn = synapseclient.login()
@@ -64,6 +80,7 @@ def clear_synapse_table(table_id):
     data = results.asDataFrame()
     syn.delete(data)
     return len(data)
+
 
 def update_synapse_table(table_id, data):
     syn = synapseclient.login()
@@ -76,7 +93,8 @@ def update_synapse_table(table_id, data):
     syn.store(table)
     return len(data)
 
-def create_new_table_version(table_id, data, comment = ""):
+
+def create_new_table_version(table_id, data, comment=""):
     n_rm = clear_synapse_table(table_id)
     n_add = update_synapse_table(table_id, data)
     n_version = create_synapse_table_snapshot(table_id, comment)
@@ -84,27 +102,55 @@ def create_new_table_version(table_id, data, comment = ""):
 
 
 # main ----------------------------
-labels = ["cohort", "site", "phase", "current_cases", "target_cases", "adjusted_cases", "pressure", "sdv", "irr"]
+labels = [
+    "cohort",
+    "site",
+    "phase",
+    "current_cases",
+    "target_cases",
+    "adjusted_cases",
+    "pressure",
+    "sdv",
+    "irr",
+]
 final = pd.DataFrame(columns=labels)
 
 # gather production counts
-for phase in config['phase'].keys():
-    for cohort in config['phase'][phase]['cohort'].keys():
-        for site in config['phase'][phase]['cohort'][cohort].get('site',  {}).keys():
-            n_current = get_current_production_record_count(synid_table_patient=config['synapse']['bpc_patient']['id'],
-                                                            cohort=cohort,
-                                                            phase=phase,
-                                                            site=site)
+for phase in config["phase"].keys():
+    for cohort in config["phase"][phase]["cohort"].keys():
+        for site in config["phase"][phase]["cohort"][cohort].get("site", {}).keys():
+            n_current = get_current_production_record_count(
+                synid_table_patient=config["synapse"]["bpc_patient"]["id"],
+                cohort=cohort,
+                phase=phase,
+                site=site,
+            )
             n_target = get_production(config, phase, cohort, site)
             n_adjust = get_adjusted(config, phase, cohort, site)
             n_pressure = get_pressure(config, phase, cohort, site)
             n_sdv = get_sdv(config, phase, cohort, site)
             n_irr = get_irr(config, phase, cohort, site)
 
-            final = final.append(pd.Series([cohort, site, phase, n_current, n_target, n_adjust, n_pressure, n_sdv, n_irr], index=labels), ignore_index=True)
+            final = final.append(
+                pd.Series(
+                    [
+                        cohort,
+                        site,
+                        phase,
+                        n_current,
+                        n_target,
+                        n_adjust,
+                        n_pressure,
+                        n_sdv,
+                        n_irr,
+                    ],
+                    index=labels,
+                ),
+                ignore_index=True,
+            )
 
 # sort
-final = final.sort_values(by=['phase', 'cohort', 'site'])
+final = final.sort_values(by=["phase", "cohort", "site"])
 
 # save ------------------------
 if save_synapse:
@@ -117,7 +163,9 @@ else:
 
 # close out ----------------------------
 if save_synapse:
-    print(f"Table saved to Synapse as 'Case Selection Counts' ({config['synapse']['case_selection']['id']}), version {n_version}")
+    print(
+        f"Table saved to Synapse as 'Case Selection Counts' ({config['synapse']['case_selection']['id']}), version {n_version}"
+    )
 else:
     print("Table saved locally to 'case_selection_counts.csv'")
 toc = time.time()
--- scripts/case_selection/utils.py
+++ scripts/case_selection/utils.py
@@ -8,6 +8,7 @@ from typing import List
 syn = synapseclient.Synapse()
 syn.login()
 
+
 def waitifnot(cond: bool, msg: List[str] = []):
     if not cond:
         for str in msg:
@@ -16,32 +17,44 @@ def waitifnot(cond: bool, msg: List[str] = []):
         while True:
             pass
 
+
 def get_sites_in_config(config: dict, phase: str, cohort: str):
-    if phase == '1_additional':
-        return list(config['phase'][1]['cohort'][cohort]['site'].keys())
+    if phase == "1_additional":
+        return list(config["phase"][1]["cohort"][cohort]["site"].keys())
     else:
-        return list(config['phase'][phase]['cohort'][cohort]['site'].keys())
+        return list(config["phase"][phase]["cohort"][cohort]["site"].keys())
+
 
 def get_default_global(config: dict, key: str):
-    return config['default']['global'][key]
+    return config["default"]["global"][key]
+
 
 def get_default_site(config: dict, site: str, key: str):
-    return config['default']['site'][site].get(key, None)
+    return config["default"]["site"][site].get(key, None)
+
 
 def get_custom(config: dict, phase: str, cohort: str, site: str, key: str):
-    return config['phase'][phase]['cohort'][cohort]['site'][site].get(key, None)
+    return config["phase"][phase]["cohort"][cohort]["site"][site].get(key, None)
+
 
 def get_production(config: dict, phase: str, cohort: str, site: str):
     return get_custom(config, phase, cohort, site, "production")
 
+
 def get_adjusted(config: dict, phase: str, cohort: str, site: str):
     return get_custom(config, phase, cohort, site, "adjusted")
 
+
 def get_pressure(config: dict, phase: str, cohort: str, site: str):
-    n_pressure_default = config['default']['site'][site]['pressure']
-    n_pressure_specific = config['phase'][phase]['cohort'][cohort]['site'][site].get('pressure', None)
-    n_pressure = n_pressure_specific if n_pressure_specific is not None else n_pressure_default
+    n_pressure_default = config["default"]["site"][site]["pressure"]
+    n_pressure_specific = config["phase"][phase]["cohort"][cohort]["site"][site].get(
+        "pressure", None
+    )
+    n_pressure = (
+        n_pressure_specific if n_pressure_specific is not None else n_pressure_default
+    )
     return n_pressure
+
 
 def get_sdv_or_irr_value(config: dict, phase: str, cohort: str, site: str, key: str):
     n_pressure = get_pressure(config, phase, cohort, site)
@@ -68,48 +81,76 @@ def get_sdv_or_irr_value(config: dict, phase: str, cohort: str, site: str, key: 
         return val_global
     return None
 
+
 def get_sdv(config: dict, phase: str, cohort: str, site: str):
     return get_sdv_or_irr_value(config, phase, cohort, site, "sdv")
+
 
 def get_irr(config: dict, phase: str, cohort: str, site: str):
     return get_sdv_or_irr_value(config, phase, cohort, site, "irr")
 
+
 def now(timeOnly: bool = False, tz: str = "US/Pacific"):
-    os.environ['TZ'] = tz
+    os.environ["TZ"] = tz
     time.tzset()
     if timeOnly:
         return time.strftime("%H:%M:%S", time.localtime())
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-def get_synapse_folder_children(synapse_id: str, include_types: List[str] = ["folder", "file", "table", "link", "entityview", "dockerrepo"]):
+
+def get_synapse_folder_children(
+    synapse_id: str,
+    include_types: List[str] = [
+        "folder",
+        "file",
+        "table",
+        "link",
+        "entityview",
+        "dockerrepo",
+    ],
+):
     ent = list(syn.getChildren(synapse_id, includeTypes=include_types))
     children = {}
     if len(ent) > 0:
         for i in range(len(ent)):
-            children[ent[i]['name']] = ent[i]['id']
+            children[ent[i]["name"]] = ent[i]["id"]
     return children
+
 
 def get_folder_synid_from_path(synid_folder_root: str, path: str):
     synid_folder_current = synid_folder_root
-    subfolders = path.split('/')
+    subfolders = path.split("/")
     for i in range(len(subfolders)):
-        synid_folder_children = get_synapse_folder_children(synid_folder_current, include_types=["folder"])
+        synid_folder_children = get_synapse_folder_children(
+            synid_folder_current, include_types=["folder"]
+        )
         if subfolders[i] not in synid_folder_children:
             return None
         synid_folder_current = str(synid_folder_children[subfolders[i]])
     return synid_folder_current
 
+
 def get_file_synid_from_path(synid_folder_root: str, path: str):
-    path_part = path.split('/')
+    path_part = path.split("/")
     file_name = path_part[-1]
-    path_abbrev = '/'.join(path_part[:-1])
+    path_abbrev = "/".join(path_part[:-1])
     synid_folder_dest = get_folder_synid_from_path(synid_folder_root, path_abbrev)
-    synid_folder_children = get_synapse_folder_children(synid_folder_dest, include_types=["file"])
+    synid_folder_children = get_synapse_folder_children(
+        synid_folder_dest, include_types=["file"]
+    )
     if file_name not in synid_folder_children:
         return None
     return str(synid_folder_children[file_name])
 
-def get_synapse_entity_data_in_csv(synapse_id: str, version: int = None, sep: str = ",", na_strings: List[str] = ["NA"], header: bool = True, check_names: bool = True):
+
+def get_synapse_entity_data_in_csv(
+    synapse_id: str,
+    version: int = None,
+    sep: str = ",",
+    na_strings: List[str] = ["NA"],
+    header: bool = True,
+    check_names: bool = True,
+):
     if version is None:
         entity = syn.get(synapse_id)
     else:
