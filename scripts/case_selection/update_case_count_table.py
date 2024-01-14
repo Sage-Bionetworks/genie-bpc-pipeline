00bd3c176c7d3faf0b251d3103cf4d8ec63ef677
Thomas Yu <thomas.yu@sagebase.org>
2024-01-13 16:17:41 -0800

    Lint and add doc strings

--- scripts/case_selection/update_case_count_table.py
+++ scripts/case_selection/update_case_count_table.py
@@ -1,12 +1,12 @@
 # Python
-import pandas as pd
-import numpy as np
-import yaml
 import argparse
-import synapseclient
-import time
-from synapseclient import Table
 import os
+import time
+
+import pandas as pd
+import synapseclient
+from synapseclient import Table
+import yaml
 
 from utils import get_production, get_adjusted, get_pressure, get_sdv, get_irr
 
@@ -19,7 +19,7 @@ if not os.path.exists("config.yaml"):
 with open(f"{workdir}/config.yaml", "r") as stream:
     config = yaml.safe_load(stream)
 
-# user input --------------------------
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-s",
@@ -65,15 +65,16 @@ def get_current_production_record_count(synid_table_patient, cohort, phase, site
     return len(record_ids)
 
 
-def create_synapse_table_snapshot(table_id, comment):
-    snapshot = syn.restPOST(
-        f"/entity/{table_id}/table/transaction/commit",
-        body=f"{{'changes': [], 'snapshotOptions': {{'snapshotComment': '{comment}'}}}}",
-    )
-    return snapshot["snapshotVersionNumber"]
-
-
 def clear_synapse_table(table_id):
+    """
+    Clears a synapse table by deleting all rows.
+
+    Parameters:
+        table_id (str): The ID of the table to be cleared.
+
+    Returns:
+        int: The number of rows deleted.
+    """
     syn = synapseclient.login()
 
     results = syn.tableQuery(f"SELECT * FROM {table_id}")
@@ -83,6 +84,16 @@ def clear_synapse_table(table_id):
 
 
 def update_synapse_table(table_id, data):
+    """
+    Updates a table in the Synapse platform with new data.
+
+    Parameters:
+        table_id (str): The ID of the table to be updated.
+        data (pandas.DataFrame): The new data to be stored in the table.
+
+    Returns:
+        int: The number of rows in the updated table.
+    """
     syn = synapseclient.login()
 
     entity = syn.get(table_id)
@@ -95,9 +106,21 @@ def update_synapse_table(table_id, data):
 
 
 def create_new_table_version(table_id, data, comment=""):
-    n_rm = clear_synapse_table(table_id)
-    n_add = update_synapse_table(table_id, data)
-    n_version = create_synapse_table_snapshot(table_id, comment)
+    """
+    Create a new version of a table in Synapse.
+
+    Parameters:
+        table_id (str): The ID of the table to create a new version for.
+        data (pandas.DataFrame): The data to update the table with.
+        comment (str, optional): A comment to associate with the new version.
+
+    Returns:
+        int: The version number of the newly created version.
+    """
+    syn = synapseclient.login()
+    _ = clear_synapse_table(table_id)
+    _ = update_synapse_table(table_id, data)
+    n_version = syn.create_snapshot_version(table_id, comment=comment)
     return n_version
 
 
