4da121b87caae5ba894fca39d6b0aaf40972dd1f
Thomas Yu <thomas.yu@sagebase.org>
2024-01-15 22:27:04 -0800

    Add docstring

--- scripts/case_selection/update_case_critera_table.py
+++ scripts/case_selection/update_case_critera_table.py
@@ -54,6 +54,15 @@ file_output = "bpc_case_selection_criteria.csv"
 
 # functions ----------------------------
 def get_phases(config):
+    """
+    Return a sorted list of phases based on the given config.
+
+    Parameters:
+    - config (dict): A dictionary containing the configuration of the phases.
+
+    Returns:
+    - list: A sorted list of phases extracted from the given config.
+    """
     phases = [
         name for name in config["phase"].keys() if not name.endswith("_additional")
     ]
@@ -61,39 +70,123 @@ def get_phases(config):
 
 
 def get_cohorts(config, phase):
+    """
+    Retrieve the list of cohorts for a given phase.
+
+    Parameters:
+        config (dict): The configuration dictionary.
+        phase (str): The phase for which to retrieve the cohorts.
+
+    Returns:
+        list: The sorted list of cohorts for the given phase.
+    """
     cohorts = list(config["phase"][phase]["cohort"].keys())
     return sorted(cohorts)
 
 
 def get_sites(config, phase, cohort):
+    """
+    Get the sites for a given configuration, phase, and cohort.
+
+    Args:
+        config (dict): The configuration dictionary.
+        phase (str): The phase to retrieve sites from.
+        cohort (str): The cohort to retrieve sites from.
+
+    Returns:
+        list: A sorted list of site names.
+    """
     sites = list(config["phase"][phase]["cohort"][cohort]["site"].keys())
     return sorted(sites)
 
 
 def get_min_seq_date(config, phase, cohort, site):
+    """
+    Retrieves the minimum sequential date from the given configuration based on the specified phase, cohort, and site.
+
+    Parameters:
+        config (dict): The configuration dictionary.
+        phase (str): The phase to retrieve the sequential date from.
+        cohort (str): The cohort to retrieve the sequential date from.
+        site (str): The site to retrieve the sequential date from.
+
+    Returns:
+        str: The minimum sequential date from the configuration.
+    """
     if "date" in config["phase"][phase]["cohort"][cohort]["site"][site]:
         return config["phase"][phase]["cohort"][cohort]["site"][site]["date"]["seq_min"]
     return config["phase"][phase]["cohort"][cohort]["date"]["seq_min"]
 
 
 def get_max_seq_date(config, phase, cohort, site):
+    """
+    Retrieves the maximum sequence date based on the given configuration, phase, cohort, and site.
+
+    Parameters:
+    - config (dict): The configuration dictionary.
+    - phase (str): The phase value.
+    - cohort (str): The cohort value.
+    - site (str): The site value.
+
+    Returns:
+    - str: The maximum sequence date.
+
+    Raises:
+    - KeyError: If any of the specified keys are not found in the configuration dictionary.
+    """
     if "date" in config["phase"][phase]["cohort"][cohort]["site"][site]:
         return config["phase"][phase]["cohort"][cohort]["site"][site]["date"]["seq_max"]
     return config["phase"][phase]["cohort"][cohort]["date"]["seq_max"]
 
 
 def get_oncotree_codes(config, phase, cohort, delim=";"):
+    """
+    Generate the oncotree codes for a specific phase and cohort.
+
+    Parameters:
+        - config (dict): The configuration dictionary containing the phase, cohort, and oncotree information.
+        - phase (str): The name of the phase.
+        - cohort (str): The name of the cohort.
+        - delim (str, optional): The delimiter used to join the oncotree codes. Defaults to ";".
+
+    Returns:
+        - oncotree_str (str): A string containing the oncotree codes joined by the specified delimiter.
+    """
     oncotree_vec = config["phase"][phase]["cohort"][cohort]["oncotree"]["allowed_codes"]
     oncotree_str = delim.join(oncotree_vec)
     return oncotree_str
 
 
 def get_target_count(config, phase, cohort, site):
+    """
+    Retrieves the target count for a specific phase, cohort, and site from the given configuration.
+
+    Parameters:
+        config (dict): A dictionary representing the configuration.
+        phase (str): The phase for which to retrieve the target count.
+        cohort (str): The cohort for which to retrieve the target count.
+        site (str): The site for which to retrieve the target count.
+
+    Returns:
+        int: The target count for the specified phase, cohort, and site.
+    """
     target = config["phase"][phase]["cohort"][cohort]["site"][site]["production"]
     return int(target)
 
 
 def main():
+    """
+    Main function that performs the following tasks:
+
+    1. Logs into the synapse client.
+    2. Reads the configuration file.
+    3. Gathers case selection criteria from the configuration file.
+    4. Retrieves phases, cohorts, sites, min_seq_date, max_seq_date, oncotree, and target count.
+    5. Creates a DataFrame to store the collected data.
+    6. Updates the case selection criteria table in Synapse if save_synapse is True.
+    7. Writes the case selection criteria to a local file if save_synapse is False.
+    8. Prints the runtime of the function.
+    """
     syn = synapseclient.login()
 
     # read ----------------------------
