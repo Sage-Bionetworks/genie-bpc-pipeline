f6ed54cb35102585fb79284a238a7867c75794d0
Thomas Yu <thomas.yu@sagebase.org>
2024-08-16 21:36:01 -0700

    Make options stopifnot

--- scripts/case_selection/workflow_case_selection.R
+++ scripts/case_selection/workflow_case_selection.R
@@ -26,7 +26,7 @@ option_list <- list(
               help="Save output to production folder")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
-waitifnot(!is.null(opt$phase) && !is.null(opt$cohort) && !is.null(opt$site),
+stopifnot(!is.null(opt$phase) && !is.null(opt$cohort) && !is.null(opt$site),
           msg = "Usage: Rscript workflow_case_selection.R -h")
 
 phase <- opt$phase
@@ -37,19 +37,19 @@ is_production <- opt$production
 # check user input -----------------
 
 phase_str <- paste0(names(config$phase), collapse = ", ")
-waitifnot(is.element(phase, names(config$phase)),
+stopifnot(is.element(phase, names(config$phase)),
           msg = c(glue("Error: phase {phase} is not valid.  Valid values: {phase_str}"),
                   "Usage: Rscript workflow_case_selection.R -h"))
 
 cohort_in_config <- names(config$phase[[phase]]$cohort)
 cohort_str <- paste0(cohort_in_config, collapse = ", ")
-waitifnot(is.element(cohort, cohort_in_config),
+stopifnot(is.element(cohort, cohort_in_config),
           msg = c(glue("Error: cohort {cohort} is not valid for phase {phase}.  Valid values: {cohort_str}"),
                   "Usage: Rscript workflow_case_selection.R -h"))
 
 sites_in_config <- get_sites_in_config(config, phase, cohort)
 site_str <- paste0(sites_in_config, collapse = ", ")
-waitifnot(is.element(site, sites_in_config),
+stopifnot(is.element(site, sites_in_config),
           msg = c(glue("Error: site {site} is not valid for phase {phase} and cohort {cohort}.  Valid values: {site_str}"),
                   "Usage: Rscript workflow_case_selection.R -h"))
 
