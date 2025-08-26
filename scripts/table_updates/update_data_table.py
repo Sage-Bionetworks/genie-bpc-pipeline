5175271098de361dab6f46f51d39d767e83891e5
danlu1 <dan.lu@sagebase.org>
2025-08-26 20:48:42 +0000

    update synapse_login function

--- scripts/table_updates/update_data_table.py
+++ scripts/table_updates/update_data_table.py
@@ -27,15 +27,8 @@ import numpy
 import pandas
 import synapseclient
 import utilities
-from synapseclient import (
-    Column,
-    Row,
-    RowSet,
-    Schema,
-    Table,
-    as_table_columns,
-    build_table,
-)
+from synapseclient import (Column, Row, RowSet, Schema, Table,
+                           as_table_columns, build_table)
 
 TABLES = {
     "production": {
@@ -524,7 +517,7 @@ def update_tier1a(
         & (column_mapping_table["cohort"] == cohort),
     ]
     valid_col = subset_column_mapping_table.prissmm_element.tolist()
-
+    import pdb; pdb.set_trace()
     if not all(item in valid_col for item in bpc_column_list):
         raise ValueError(
             f"Invalid bpc_column_list. Column names should be matching {valid_col}."
@@ -806,12 +799,6 @@ def main():
         choices=TABLES["production"].keys(),
     )
     parser.add_argument(
-        "-s",
-        "--synapse_config",
-        default=synapseclient.client.CONFIG_FILE,
-        help="Synapse credentials file",
-    )
-    parser.add_argument(
         "-p", "--project_config", default="config.json", help="Project config file"
     )
     parser.add_argument(
@@ -842,10 +829,10 @@ def main():
     )
     parser.add_argument("-m", "--message", default="", help="Version comment")
     parser.add_argument("-d", "--dry_run", action="store_true", help="dry run flag")
+    parser.add_argument("--debug", action="store_true", help="Synapse Debug Feature")
 
     args = parser.parse_args()
     table_type = args.table
-    synapse_config = args.synapse_config
     project_config = args.project_config
     cohort = args.cohort
     production = args.production
@@ -855,7 +842,7 @@ def main():
     dry_run = args.dry_run
 
     # login to synapse
-    syn = utilities.synapse_login(synapse_config)
+    syn = utilities.synapse_login(debug=args.debug)
 
     # create logger
     logger_name = "testing" if dry_run else "production"
@@ -884,7 +871,7 @@ def main():
     label_data["redacted"] = numpy.nan
 
     # update data tables
-    store_data(syn, master_table, label_data, table_type, cohort, logger, dry_run)
+    #store_data(syn, master_table, label_data, table_type, cohort, logger, dry_run)
     if not dry_run:
         if replace_patient_tier1a or replace_sample_tier1a:
             custom_fix_for_tier1a_variable(
